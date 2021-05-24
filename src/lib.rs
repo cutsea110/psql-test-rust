c3e6cb92bfd2070570e5dd2d22138a4bc680fb9c
cutsea110 <cutsea110@gmail.com>
2021-05-24 21:43:11 +0900

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -15,10 +15,7 @@ use self::models::{NewPost, Post};
 pub fn create_post<'a>(conn: &PgConnection, title: &'a str, body: &'a str) -> Post {
     use schema::posts;
 
-    let new_post = NewPost {
-        title: title,
-        body: body,
-    };
+    let new_post = NewPost { title, body };
     diesel::insert_into(posts::table)
         .values(&new_post)
         .get_result(conn)
